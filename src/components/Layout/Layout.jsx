d57d27e5cc48ed6e343a4b783100c34e30388381
Abu Hanjala <abuhanjala656@gmail.com>
2026-02-02 23:20:05 +0530

    fix the error

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '../Navbar/Navbar'
-import Footer from '../footer/footer'
+import Footer from '../Footer/Footer'
 const Layout = () => {
     return (
         <div>
